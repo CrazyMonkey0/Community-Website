724d5594719a955bdc4ee1450a2060cf0ecdf083
hubi <hubcio1765@gmail.com>
2023-08-08 18:02:14 +0200

    Creating a login view

---
+++ greenzone/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+
+
+# login form
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
--- www/settings.py
+++ www/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'greenzone.apps.GreenzoneConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
