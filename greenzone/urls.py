95d6ca16d8afc007b56d07fd7a9581249aa78c0f
hubi <hubcio1765@gmail.com>
2023-08-09 12:23:23 +0200

    Views of password change

--- greenzone/urls.py
+++ greenzone/urls.py
@@ -2,11 +2,18 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
 
-
 urlpatterns = [
     # Login Views
     # path('login/', views.user_login, name='login'),
+    # URLs for login and logout
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', views.dashboard, name='dashboard'),
+    # URLs for changing passwords.
+    path('password_change/',
+         auth_views.PasswordChangeView.as_view(),
+         name='password_change'),
+    path('password_change/done/',
+         auth_views.PasswordChangeDoneView.as_view(),
+         name='password_change_done'),
 ]
