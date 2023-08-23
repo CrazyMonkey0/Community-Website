8fd21495fe91453432135bf82354dcccd88b6b30
hubi <hubcio1765@gmail.com>
2023-08-23 14:02:29 +0200

    Using a messaging framework

--- greenzone/views.py
+++ greenzone/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 # from django.contrib.auth import authenticate, login
 from .forms import UserRegistrationForm, UserEditForm, ProfileEditForm
 from .models import Profile
@@ -73,6 +74,9 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, "The profile update was successful.")
+        else:
+            messages.error(request, "An error occurred while updating the profile.")
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
