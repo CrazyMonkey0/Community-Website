1d7adcb48217214df2a71870dd9a4cd9c4104942
hubi <hubcio1765@gmail.com>
2023-09-20 16:37:40 +0200

    Added AJAX paging to list views

--- images/views.py
+++ images/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.decorators.http import require_POST
-from common.decorators import ajax_required
+from common.decorators import ajax_required, is_ajax
 from .forms import ImageCreateForm
 from .models import Image
 
@@ -55,3 +56,25 @@ def image_like(request):
         except:
             pass
     return JsonResponse({'status':'ok'})
+
+@login_required
+def image_list(request):
+    
+    images = Image.objects.all()
+    paginator = Paginator(images, 10)
+    page = request.GET.get('page')
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        images = paginator.page(1)
+    except EmptyPage:
+        if is_ajax(request):
+            return HttpResponse('')
+        images = paginator.page(paginator.num_pages)
+    if is_ajax(request):
+        return render(request,
+               'images/image/list_ajax.html',
+               {'section': 'images', 'images': images})
+    return render(request,
+            'images/image/list.html',
+            {'section': 'images', 'images': images})
