8ca17349b9ef0f97be5d9b0681e41913e11407a7
hubi <hubcio1765@gmail.com>
2023-09-23 15:40:03 +0200

    Use of signals for denormalized counts

--- images/apps.py
+++ images/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class ImagesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'images'
+
+    def ready(self):
+        # Import of signal handling procedures.
+        import images.signals
