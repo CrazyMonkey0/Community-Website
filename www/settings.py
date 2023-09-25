5167db9c17459b79b23def3a5309fad5ed33cebe
CrazyMonkey0 <hubcio1765@gmail.com>
2023-09-25 22:22:39 +0200

    Adding postgresql as the primary database

--- www/settings.py
+++ www/settings.py
@@ -79,12 +79,13 @@ WSGI_APPLICATION = 'www.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        'OPTIONS': {
+            "service": "my_service",
+        }
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -140,13 +141,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 
 AUTHENTICATION_BACKENDS = [
-'django.contrib.auth.backends.ModelBackend',
-'greenzone.authentication.EmailAuthBackend',
+    'django.contrib.auth.backends.ModelBackend',
+    'greenzone.authentication.EmailAuthBackend',
 ]
 
 ABSOLUTE_URL_OVERRIDES = {
-'auth.user': lambda u: reverse_lazy('user_detail',
-                                    args=[u.username])
+    'auth.user': lambda u: reverse_lazy('user_detail',
+                                        args=[u.username])
 }
 
 
